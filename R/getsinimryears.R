bf839f515306130c79a069e43fd186ade8ae4951
Roberto Salas <wala.rsalas@gmail.com>
2018-09-11 17:51:24 -0300

    Update getsinimryears.R

--- R/getsinimryears.R
+++ R/getsinimryears.R
@@ -11,7 +11,7 @@
 #' @import reshape2
 
 getsinimryears <- function(var, years) {
-  if ((class(var) != "numeric") | (class(years) != "numeric") | (length(years)<2)) {
+  if ((class(var) != "integer") | (class(years) != "integer") | (length(years)<2)) {
     stop("Variables must be numeric or you have to add more years to retrieve information")
   } else {
     list <- lapply(years, function(x)
