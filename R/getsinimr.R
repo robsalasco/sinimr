05c9f80de5225dd37b8da891b56e896d07f14dba
Roberto Salas <wala.rsalas@gmail.com>
2018-09-11 20:02:12 -0300

    Update getsinimr.R

--- R/getsinimr.R
+++ R/getsinimr.R
@@ -11,7 +11,7 @@
 #' @import XML
 #' @import reshape2
 
-getsinimr <- function(var, year) {
+getsinimr <- function(var, year, corrmon = TRUE) {
   if (!is.numeric(var) | !is.numeric(year)) {
     stop("Variables must be numeric")
   } else {
@@ -22,7 +22,7 @@ getsinimr <- function(var, year) {
         paste(var, collapse = ","),
         "&periodos[]=",
         year,
-        "&regiones[]=T&municipios[]=T&corrmon=1",
+        "&regiones[]=T&municipios[]=T&corrmon=", as.numeric(corrmon),
         sep = ""
       )
     data <- xmlParse(callapi(url))
