8107027cc8ebc13d3d277c448226eecba154ff01
Roberto Salas <wala.rsalas@gmail.com>
2018-10-05 22:54:20 -0300

    fix typo

--- R/sinimr.R
+++ R/sinimr.R
@@ -1,7 +1,7 @@
 #' @name sinimr
 #' @docType package
 #' @title R library for SINIM
-#' @description: Access SINIM data and view it as data frames.
+#' @description Access SINIM data and view it as data frames.
 #' @author Roberto Salas \email{rosalas@uc.cl}
 #' @keywords package
 #' @import httr
