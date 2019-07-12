bce9160b7153b7a39d8ab484007b9d4f1a34d316
Roberto Salas <wala.rsalas@gmail.com>
2019-07-12 15:24:19 -0400

    output all variables if no keyword

--- R/searchsinimvar.R
+++ R/searchsinimvar.R
@@ -7,7 +7,6 @@
 
 searchsinimvar <- function(keyword) {
     
-    stopifnot(is.character(keyword))
   
     body <- list("dato_area[]" = "T", "dato_subarea[]" = "T")
     resp2 <- postapi("http://datos.sinim.gov.cl/datos_municipales/obtener_datos_filtros.php",
@@ -15,7 +14,14 @@ searchsinimvar <- function(keyword) {
     data <- Reduce(function(...) merge(..., all=T), resp2)
     data <- data[ ,c(8, 10, 18, 2, 4, 5)]
     colnames(data) <- c("CODE", "VARIABLE", "DESCRIPTION", "AREA", "SUBAREA", "UNIT")
-    search <- data[with(data, grepl(keyword, paste(VARIABLE, DESCRIPTION, AREA, SUBAREA), ignore.case = T)), ]
+    
+    if (missing(keyword)) {
+      search <- data
+    } 
+    else {
+      stopifnot(is.character(keyword))
+      search <- data[with(data, grepl(keyword, paste(VARIABLE, DESCRIPTION, AREA, SUBAREA), ignore.case = T)), ]
+    }
+    
     return(search)
-
 }
