0b1cdd141419049ccd1626e9391c5fdbf9063a91
Roberto Salas <wala.rsalas@gmail.com>
2019-08-31 04:18:40 -0400

    updated functions, added geometries and renamed files

---
+++ R/get_sinim.R
@@ -0,0 +1,145 @@
+#' Get a SINIM variable data in a specific year as a data frame
+#' @param var Variable code
+#' @param year Year
+#' @param moncorr A logical value indicating the use of monetary correction
+#' @param idgeo A logical value to add province and region columns
+#' @param geometry A logical value to add geographical features
+#' @param region Regional subsetting variable
+#' @param province Province subsetting variable
+#' @param comuna Comuna subsetting variable
+#' @return data frame with data for the requested variable over time with optional geometry
+#' @export
+#' @examples
+#' get_sinim(880, 2015)
+#' get_sinim(880, 2015:2017)
+#' get_sinim(c(880, 882), 2015)
+#' get_sinim(c(880, 882), 2015:2017)
+#' get_sinim(c(880, 882), 2015, idgeo=TRUE)
+#' get_sinim(882, 2015, geometry=TRUE)
+#' get_sinim(882, 2015, region="02")
+#' @importFrom reshape2 melt
+#' @importFrom stats reshape
+#' @importFrom stats na.omit
+#' @import sf
+#'
+get_sinim <-
+  function(var,
+           year,
+           moncorr = T,
+           idgeo = F,
+           geometry = F,
+           region,
+           province,
+           comuna) {
+    
+    stopifnot(is.numeric(var))
+    stopifnot(is.numeric(year))
+    
+    values <- parsexml(var, year, moncorr = moncorr)
+    
+    colnames(values) <-
+      c("CODE", "MUNICIPALITY", namesco(var, year))
+    
+    datav <- stats::reshape(
+      values,
+      idvar = c("CODE", "MUNICIPALITY"),
+      varying = namesco(var, year),
+      direction = "long",
+      timevar = "YEAR",
+      times = sort(year, decreasing = T)
+    )
+    
+    rownames(datav) <- NULL
+    
+    datav <-
+      as.data.frame(apply(datav, 2, function(x)
+        gsub("No Recepcionado|No Aplica", NA, x)), stringsAsFactors = FALSE)
+    
+    if (geometry == FALSE) {
+      datav <- reshape2::melt(
+        datav,
+        id = c("CODE", "MUNICIPALITY", "YEAR"),
+        value.name = "VALUE",
+        variable.name = "VARIABLE"
+      )
+      
+      t <-
+        vapply(datav, function(x)
+          all(grepl("[0-9]+", na.omit(x))), logical(1))
+      t[1:3] <- FALSE
+      datav[t] <- lapply(datav[t], as.numeric)
+      
+      if (!missing(region) |
+          !missing(province) | !missing(comuna)) {
+        selection <-
+          geofilter(region = region,
+                    province = province,
+                    comuna = comuna)
+        data.selection <- subset(datav, CODE %in% selection)
+        
+        if (idgeo == T) {
+          merged.pretty <- merge(data.selection, id_geo_census, by = "CODE")
+          return(merged.pretty)
+        } else {
+          return(data.selection)
+        }
+        
+      } else {
+        if (idgeo == T) {
+          merged.pretty <- merge(datav, id_geo_census, by = "CODE")
+          return(merged.pretty)
+        } else {
+          return(datav)
+        }
+      }
+    } else {
+      t <-
+        vapply(datav, function(x)
+          all(grepl("[0-9]+", na.omit(x))), logical(1))
+      t[1:3] <- FALSE
+      datav[t] <- lapply(datav[t], as.numeric)
+      
+      if (!missing(region) |
+          !missing(province) | !missing(comuna)) {
+        selection <-
+          geofilter(region = region,
+                    province = province,
+                    comuna = comuna)
+        data.reshaped <-
+          reshape(
+            datav,
+            idvar = c("CODE", "MUNICIPALITY"),
+            timevar = "YEAR",
+            direction = "wide"
+          )
+        data.selection <- subset(data.reshaped, CODE %in% selection)
+        merged.geo <-
+          merge(census_geometry, data.selection, by = "CODE")
+        
+        if (idgeo == T) {
+          merged.geo.pretty <- merge(merged.geo, id_geo_census, by = "CODE")
+          return(merged.geo.pretty)
+        } else {
+          return(merged.geo)
+        }
+        
+      } else {
+        data.reshaped <-
+          reshape(
+            datav,
+            idvar = c("CODE", "MUNICIPALITY"),
+            timevar = "YEAR",
+            direction = "wide"
+          )
+        merged.geo <-
+          merge(census_geometry, data.reshaped, by = "CODE")
+        
+        if (idgeo == T) {
+          merged.geo.pretty <- merge(merged.geo, id_geo_census, by = "CODE")
+          return(merged.geo.pretty)
+        } else {
+          return(merged.geo)
+        }
+      }
+    }
+  }
