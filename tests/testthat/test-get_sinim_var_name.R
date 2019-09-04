d8d21fc67d6fa57c04ae561da5feba989726d73d
Roberto Salas <wala.rsalas@gmail.com>
2019-09-04 16:08:46 -0400

    updated tests

---
+++ tests/testthat/test-get_sinim_var_name.R
@@ -0,0 +1,8 @@
+context("test-sinimr.R")
+
+test_that("get_sinim_var_name returns a valid output", {
+  
+  varname <- get_sinim_var_name("ingreso")
+  
+  expect_is(srch, "character")
+})
---
+++ tests/testthat/test-mm.R
@@ -0,0 +1,8 @@
+context("test-sinimr.R")
+
+test_that("mm returns a valid output", {
+  
+  varname <- mm(5807587000)
+  
+  expect_is(srch, "character")
+})
