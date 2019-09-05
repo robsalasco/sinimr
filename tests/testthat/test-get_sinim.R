df7e29a580df68fe87cff4c798f17a889e4fa8ed
Roberto Salas <wala.rsalas@gmail.com>
2019-09-04 20:59:03 -0400

    new tests

--- tests/testthat/test-get_sinim.R
+++ tests/testthat/test-get_sinim.R
@@ -49,3 +49,28 @@ test_that("get_sinim returns a sf object", {
   
   expect_is(tbl, c("sf", "data.frame"))
 })
+
+test_that("get_sinim returns a valid output using a region subset", {
+  
+  tbl <-  get_sinim(c(4333, 4211), 2015, region=1)
+  
+  expect_is(tbl, c("data.frame"))
+  expect_output(str(tbl), "14 obs. of  5 variables")
+})
+
+test_that("get_sinim returns a valid output using a provincia subset", {
+  
+  tbl <-  get_sinim(c(4333, 4211), 2015, provincia=14)
+  
+  expect_is(tbl, c("data.frame"))
+  expect_output(str(tbl), "10 obs. of  5 variables")
+})
+
+test_that("get_sinim returns a valid output using a comuna subset", {
+  
+  tbl <-  get_sinim(c(4333, 4211), 2015, comuna=13123)
+  
+  expect_is(tbl, c("data.frame"))
+  expect_output(str(tbl), "2 obs. of  5 variables")
+})
+
