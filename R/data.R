5041d15cbe990a676f8314954b01fefeb04408e3
Roberto Salas <wala.rsalas@gmail.com>
2022-09-12 20:47:23 -0300

    data docs

---
+++ R/data.R
@@ -0,0 +1,40 @@
+#' Chilean administrative divisions 
+#'
+#' A dataset containing the chilean administrative units.
+#'
+#' @format A data frame with 345 rows and 7 variables:
+#' \describe{
+#'   \item{code}{municipality code}
+#'   \item{municipality}{municipality name}
+#'   \item{code.reg}{region code}
+#'   \item{nom.reg}{region name}
+#'   \item{code.prov}{province code}
+#'   \item{nom.prov}{province name}
+#'   \item{auc}{yes/no consolidated urban area}
+#' }
+#' @source \url{http://www.ine.cl/}
+"id_geo_census"
+
+#' Chilean municipalities polygons as simple features 
+#'
+#' An sf object containing the chilean municipalities.
+#'
+#' @format A data frame with 345 rows and 2 variables:
+#' \describe{
+#'   \item{code}{municipality code}
+#'   \item{geometry}{municipality geometry}
+#' }
+#' @source \url{http://www.ine.cl/}
+"census_geometry_comunas"
+
+#' Chilean municipalities urban boundaries polygons as simple features 
+#'
+#' An sf object containing the chilean municipalities urban boundaries.
+#'
+#' @format A data frame with 220 rows and 2 variables:
+#' \describe{
+#'   \item{code}{municipality code}
+#'   \item{geometry}{municipality geometry}
+#' }
+#' @source \url{http://www.ine.cl/}
+"census_geometry_limites"
