70e98c81a009e97875f3fc952d4a685066e29777
Roberto Salas <wala.rsalas@gmail.com>
2023-03-03 11:03:47 -0300

    updated test

--- tests/testthat/test-search_sinim_vars.R
+++ tests/testthat/test-search_sinim_vars.R
@@ -5,5 +5,5 @@ test_that("search_sinim_vars returns a valid output", {
   srch <- search_sinim_vars("ingreso")
   
   expect_is(srch, "data.frame")
-  expect_output(str(srch), "80 obs. of  6 variables")
+  expect_output(str(srch), "81 obs. of  6 variables")
 })
