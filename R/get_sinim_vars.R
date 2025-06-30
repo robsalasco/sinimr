1026bf10bf6c0a52c3f1b477b6606cdcd766639c
Roberto Salas <rsalas@goplaceit.com>
2025-06-30 16:29:28 -0400

    oops

--- R/get_sinim_vars.R
+++ R/get_sinim_vars.R
@@ -12,7 +12,7 @@ get_sinim_vars <- function(catn) {
   
   body <- list("dato_area[]" = "T", "dato_subarea[]" = "T")
   resp <- postapi(
-    "http://datos.sinim.gov.cl/datos_municipales/obtener_datos_filtros.php",
+    "https://datos.sinim.gov.cl/datos_municipales/obtener_datos_filtros.php",
     body
   )
   list <- Reduce(function(...) merge(..., all = T), resp)
