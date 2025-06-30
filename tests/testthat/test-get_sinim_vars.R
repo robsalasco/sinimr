5dd4ab4a6d8262576c581539c120ac9611e21365
Roberto Salas <rsalas@goplaceit.com>
2025-06-30 16:39:00 -0400

    updated test

--- tests/testthat/test-get_sinim_vars.R
+++ tests/testthat/test-get_sinim_vars.R
@@ -5,5 +5,5 @@ test_that("get_sinim_vars returns a valid output", {
   tbl <- get_sinim_vars(262)
   
   expect_is(tbl, "data.frame")
-  expect_output(str(tbl), "4 obs. of  3 variables")
+  expect_output(str(tbl), "22 obs. of  3 variables")
 })
